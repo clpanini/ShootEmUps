1f930d64e48bf0120b2025bb6fd95311085591d5
clpanini <chloelainedpangilinan@gmail.com>
2026-02-23 20:56:10 +0800

    Added Lasers

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -9,9 +9,14 @@ extends Node2D
 @onready var hurtbox_component:  = $HurtboxComponent as HurtboxComponent
 @onready var hitbox_component:  = $HitboxComponent as HitboxComponent
 @onready var destroyed_component:  = $DestroyedComponent as DestroyedComponent
+@onready var left_laser: Marker2D = $LeftLaser
+@onready var right_laser: Marker2D = $"RightLaser"
+@onready var spawner_component_2: SpawnerComponent = $SpawnerComponent2 as SpawnerComponent
+@onready var laser_rate_timer: Timer = $LaserRateTimer
 
-
+	
 func _ready() -> void:
+	laser_rate_timer.timeout.connect(shoot_lasers)
 	visible_on_screen_notifier_2d.screen_exited.connect(queue_free)
 	hurtbox_component.hurt.connect(func(hitbox: HitboxComponent):
 		scale_component.tween_scale()
@@ -20,3 +25,8 @@ func _ready() -> void:
 	)
 	stats_component.no_health.connect(queue_free)
 	hitbox_component.hit_hurtbox.connect(destroyed_component.destroy.unbind(1))
+
+func shoot_lasers() -> void:
+	spawner_component_2.spawn(left_laser.global_position)
+	spawner_component_2.spawn(right_laser.global_position)
+	scale_component.tween_scale()
