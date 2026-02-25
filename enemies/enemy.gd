bed5b44e82a6a720a079cf43ceb8003c977bf60e
clpanini <chloelainedpangilinan@gmail.com>
2026-02-25 12:12:44 +0800

    Fixed Laser and Collisions Logic

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -1,34 +1,44 @@
 extends Node2D
 
-@onready var stats_component:  = $StatsComponent as StatsComponent
-@onready var move_component:  = $MoveComponent as MoveComponent
+@onready var stats_component: StatsComponent = $StatsComponent as StatsComponent
+@onready var move_component: MoveComponent = $MoveComponent as MoveComponent
 @onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
-@onready var scale_component:  = $ScaleComponent as ScaleComponent
-@onready var flash_component:  = $FlashComponent as FlashComponent
-@onready var shake_component:  = $ShakeComponent as ShakeComponent
-@onready var hurtbox_component:  = $HurtboxComponent as HurtboxComponent
-@onready var hitbox_component:  = $HitboxComponent as HitboxComponent
-@onready var destroyed_component:  = $DestroyedComponent as DestroyedComponent
+@onready var scale_component: ScaleComponent = $ScaleComponent as ScaleComponent
+@onready var flash_component: FlashComponent = $FlashComponent as FlashComponent
+@onready var shake_component: ShakeComponent = $ShakeComponent as ShakeComponent
+@onready var hurtbox_component: HurtboxComponent = $HurtboxComponent as HurtboxComponent
+@onready var hitbox_component: HitboxComponent = $HitboxComponent as HitboxComponent
+@onready var destroyed_component: DestroyedComponent = $DestroyedComponent as DestroyedComponent
 @onready var enemy_laser: Marker2D = $EnemyLaser 
 @onready var spawner_component_2: SpawnerComponent = $SpawnerComponent2 as SpawnerComponent
 @onready var laser_rate_timer: Timer = $LaserRateTimer
-@onready var score_component:  = $ScoreComponent as ScoreComponent
+@onready var score_component: ScoreComponent = $ScoreComponent as ScoreComponent
 
-	
 func _ready() -> void:
-	stats_component.no_health.connect(func():
-		score_component.adjust_score()
-	)
-	
 	laser_rate_timer.timeout.connect(shoot_lasers)
 	visible_on_screen_notifier_2d.screen_exited.connect(queue_free)
+	
 	hurtbox_component.hurt.connect(func(hitbox: HitboxComponent):
+		stats_component.health -= 1
+		flash_component.flash()
+		shake_component.tween_shake()
 		scale_component.tween_scale()
+		
+		# This correctly deletes the player's laser when it hits the enemy
+		if hitbox.owner is Node2D and hitbox.owner.name.containsn("laser"):
+			hitbox.owner.queue_free()
+	)
+	
+	hitbox_component.hit_hurtbox.connect(func(hurtbox: HurtboxComponent):
+		stats_component.health -= 1
 		flash_component.flash()
 		shake_component.tween_shake()
 	)
-	stats_component.no_health.connect(queue_free)
-	hitbox_component.hit_hurtbox.connect(destroyed_component.destroy.unbind(1))
+	
+	stats_component.no_health.connect(func():
+		score_component.adjust_score()
+		destroyed_component.destroy() 
+	)
 
 func shoot_lasers() -> void:
 	spawner_component_2.spawn(enemy_laser.global_position)
--- player_ship/ship.gd
+++ player_ship/ship.gd
@@ -11,15 +11,45 @@ extends Node2D
 @onready var move_component: MoveComponent = $MoveComponent as MoveComponent
 @onready var flame_animation: AnimatedSprite2D = $Anchor/FlameAnimation
 
+@onready var stats_component: StatsComponent = $StatsComponent as StatsComponent
+@onready var hurtbox_component: HurtboxComponent = $HurtboxComponent as HurtboxComponent
+@onready var flash_component = $FlashComponent 
+@onready var shake_component = $ShakeComponent
+@onready var hitbox_component = $HitboxComponent
 
 func _ready() -> void:
-	pass 
+	hurtbox_component.hurt.connect(func(hitbox: HitboxComponent):
+		stats_component.health -= 1
+		if flash_component:
+			flash_component.flash()
+		if shake_component:
+			shake_component.tween_shake()
+		if hitbox.owner is Node2D:
+			hitbox.owner.queue_free()
+	)
+	
+	if hitbox_component:
+		hitbox_component.hit_hurtbox.connect(func(hurtbox: HurtboxComponent):
+			stats_component.health -= 1
+			if flash_component:
+				flash_component.flash()
+			if shake_component:
+				shake_component.tween_shake()
+	)
+	
+
+	stats_component.no_health.connect(func():
+		get_tree().change_scene_to_file("res://scenes/game_over.tscn")
+	)
 
 func _process(delta: float) -> void:
 	handle_movement()
 	handle_shooting()
 	animate_the_ship()
-
+	
+	var view_rect = get_viewport_rect().size
+	global_position.x = clamp(global_position.x, 8, view_rect.x - 8)
+	global_position.y = clamp(global_position.y, 8, view_rect.y - 8)
 
 func handle_movement() -> void:
 	var direction = Vector2.ZERO
@@ -32,12 +62,10 @@ func handle_movement() -> void:
 	move_component.velocity = direction * ship_speed
 
 func handle_shooting() -> void:
-
-	if Input.is_action_pressed("ui_accept"):
-	
+	if Input.is_action_pressed("ui_accept"): 
 		if shoot_rate_timer.is_stopped():
 			shoot_lasers()
-			shoot_rate_timer.start() 
+			shoot_rate_timer.start()
 
 func shoot_lasers() -> void:
 	spawner_component.spawn(left_gun.global_position)
@@ -45,7 +73,6 @@ func shoot_lasers() -> void:
 	scale_component.tween_scale()
 	
 func animate_the_ship() -> void:
-
 	if move_component.velocity.x < 0:
 		animated_sprite_2d.play("move_left")
 		flame_animation.play("move_left")
