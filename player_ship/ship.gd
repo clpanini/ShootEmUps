ba3cce4235827ba82c0db6ee7929ff9135aee936
clpanini <chloelainedpangilinan@gmail.com>
2026-02-25 10:54:53 +0800

    Ship can move around and spawn its own lasers

--- player_ship/ship.gd
+++ player_ship/ship.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@export var ship_speed: float = 300.0 
+
 @onready var left_gun: Marker2D = $LeftGun
 @onready var right_gun: Marker2D = $RightGun
 @onready var spawner_component: SpawnerComponent = $SpawnerComponent as SpawnerComponent
@@ -9,20 +11,41 @@ extends Node2D
 @onready var move_component: MoveComponent = $MoveComponent as MoveComponent
 @onready var flame_animation: AnimatedSprite2D = $Anchor/FlameAnimation
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	shoot_rate_timer.timeout.connect(shoot_lasers)
-	pass # Replace with function body.
+	pass 
+
+func _process(delta: float) -> void:
+	handle_movement()
+	handle_shooting()
+	animate_the_ship()
+
+
+func handle_movement() -> void:
+	var direction = Vector2.ZERO
+	direction.x = Input.get_axis("ui_left", "ui_right")
+	direction.y = Input.get_axis("ui_up", "ui_down")
+	
+	if direction.length() > 0:
+		direction = direction.normalized()
+	
+	move_component.velocity = direction * ship_speed
+
+func handle_shooting() -> void:
+
+	if Input.is_action_pressed("ui_accept"):
+	
+		if shoot_rate_timer.is_stopped():
+			shoot_lasers()
+			shoot_rate_timer.start() 
 
 func shoot_lasers() -> void:
 	spawner_component.spawn(left_gun.global_position)
 	spawner_component.spawn(right_gun.global_position)
 	scale_component.tween_scale()
 	
-func _process(delta: float) -> void:
-	animate_the_ship()
-	
 func animate_the_ship() -> void:
+
 	if move_component.velocity.x < 0:
 		animated_sprite_2d.play("move_left")
 		flame_animation.play("move_left")
@@ -32,4 +55,3 @@ func animate_the_ship() -> void:
 	else:
 		animated_sprite_2d.play("center")
 		flame_animation.play("center")
-		
